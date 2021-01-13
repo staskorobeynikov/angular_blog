fb44a1aa27428dd7fd971ac119ca2bdd2c9c577b
staskorobeynikov <stas.korobeinikov@mail.ru>
2021-01-13 19:51:50 +0300

    Реализация блога. Часть 1[#1]

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { PostComponent } from './shared/components/post/post.component';
+import {AuthService} from './shared/services/auth.service';
+import {SharedModule} from '../shared/shared.module';
+
+@NgModule({
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent, PostComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+          {path: 'login', component: LoginPageComponent},
+          {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'create', component: CreatePageComponent},
+          {path: 'post/:id/edit', component: EditPageComponent}
+        ]
+      }
+    ])
+  ],
+  exports: [RouterModule, PostComponent],
+  providers: [AuthService]
+})
+export class AdminModule {
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCS7YycjmqZr9ZIKEZ_Vnun8REQPC8oSPw'
 };
